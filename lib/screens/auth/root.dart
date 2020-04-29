b5c716a12b71dec311dbcd5b4ae5a8165713d006
ROHIT BAKOLIYA <46809038+rohitbakoliya@users.noreply.github.com>
2020-04-29 14:50:36 +0530

    Update root.dart

--- lib/screens/auth/root.dart
+++ lib/screens/auth/root.dart
@@ -1,7 +1,7 @@
-import 'package:auth/screens/auth/signIn.dart';
-import 'package:auth/screens/home/home.dart';
+import 'package:auth/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 class Root extends StatefulWidget {
   @override
@@ -9,28 +9,40 @@ class Root extends StatefulWidget {
 }
 
 class _RootState extends State<Root> {
-  bool isLogin = false;
-  FirebaseUser _user;
-  @override
-  void initState() {
-    super.initState();
-    currentUser().then((user) =>{
-      setState(
-        (){
-          isLogin = user.uid==null  ? false : true;
-          _user = user;
-        }
-      )
-    } );
-  }
 
+  Stream<FirebaseUser> get user{
+    return FirebaseAuth.instance.onAuthStateChanged;
+  }
   @override
   Widget build(BuildContext context) {
-    return isLogin ? Home(user: _user) : SignIn();
+    return StreamProvider<FirebaseUser>.value(
+      value: user,
+      child: Wrapper(),
+    );
   }
-  Future< FirebaseUser > currentUser() async{
-    FirebaseUser user =  await FirebaseAuth.instance.currentUser();
-    return user;
-  }
+  // bool isLogin = false;
+  // FirebaseUser _user;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   currentUser().then((user) =>{
+  //     setState(
+  //       (){
+  //         isLogin = user.uid==null  ? false : true;
+  //         _user = user;
+  //       }
+  //     )
+  //   } );
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   print('here loginis $isLogin');
+  //   return isLogin ? Home(user: _user) : SignIn();
+  // }
+  // Future< FirebaseUser > currentUser() async{
+  //   FirebaseUser user =  await FirebaseAuth.instance.currentUser();
+  //   return user;
+  // }
 
 }
